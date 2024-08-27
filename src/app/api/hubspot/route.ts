03448e52310a24424e3b6d55ad2a2227fafb623d
mon <raquindoza14@gmail.com>
2024-08-27 17:57:55 +0800

    log request

--- src/app/api/hubspot/route.ts
+++ src/app/api/hubspot/route.ts
@@ -1,3 +1,7 @@
-export async function GET() {
+import docusign from "docusign-esign";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  console.log("request", request);
   return Response.json({ data: { hello: "hello" } });
 }
